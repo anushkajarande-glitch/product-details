f13246ed433eb13e762dc700011bcf840d8fd717
Anushka Jarande <anushkajarande@gmail.com>
2026-04-25 14:35:32 +0530

    fixed port for render deployment

--- server.js
+++ server.js
@@ -1,20 +1,5 @@
-const http = require('http');
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/product") {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-            id: 101,
-            name: "Laptop",
-            price: 50000,
-            category: "Electronics"
-        }));
-    } else {
-        res.writeHead(404);
-        res.end("Route Not Found");
-    }
-});
-
-server.listen(3000, () => {
-    console.log("Product Details Microservice running on port 3000");
+server.listen(PORT, () => {
+    console.log(`Product Details Microservice running on port ${PORT}`);
 });
